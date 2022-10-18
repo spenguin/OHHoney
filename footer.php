4d9e3b588604628f71beba5d543518093fe703c5
John Anderson <info@soaringpenguin.com>
2022-10-17 19:20:21 -0700

    Remove most of skeleton theme.

--- footer.php
+++ footer.php
@@ -10,7 +10,22 @@
  */
 
 ?>
+<footer>
+	<div class="footer-wrapper max-wrapper">
+		<div class="footer-wrapper__copyright">
+			<p>Copyright</p>
+		</div>
+		<div class="footer-wrapper__instagram">
+			<?php dynamic_sidebar( 'footer-middle' ); ?>
+			<p>Instagram</p>
+		</div>
+		<div class="footer-wrapper__signup">
+			<p>MailChimp</p>
+		</div>
+	</div>
+</footer>
 
+<!-- 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ohhoney' ) ); ?>">
@@ -25,10 +40,10 @@
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ohhoney' ), 'ohhoney', '<a href="http://underscores.me/">Underscores.me</a>' );
 				?>
 		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<!-- </footer>#colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
-</html>
+</html> 
