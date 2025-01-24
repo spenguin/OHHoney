58cdc545fae755dd68854944a394b64e93f46134
John Anderson <info@soaringpenguin.com>
2025-01-24 14:21:41 -0800

    Reinstate code for Home Page and Products page.

---
+++ template-parts/header-social-media.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Social Media
+ */
+?>
+<div class="header-icon-container">
+    <a href="https://www.facebook.com/occasionallyhoney/" target="_blank">
+        <i class="fab fa-facebook-square icon-color"></i>
+    </a>
+    <a href="https://www.instagram.com/occasionallyhoney/" target="_blank">
+        <i class="fab fa-instagram icon-color"></i>
+    </a>
+    <a href="https://www.occasionallyhoney.com/cart">
+        <i class="fas fa-shopping-cart icon-color"></i>
+    </a>
+</div>
