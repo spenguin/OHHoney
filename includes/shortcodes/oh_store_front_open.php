b89bb5ed813659390f7f124940ef74604029e2b2
John Anderson <info@soaringpenguin.com>
2025-06-26 12:30:59 -0700

    Integrate Shop Opening Hours with Home Page.

--- includes/shortcodes/oh_store_front_open.php
+++ includes/shortcodes/oh_store_front_open.php
@@ -7,16 +7,30 @@ function oh_store_front_open( $atts = [], $content = null, $tag = '' )
 { 
     $content        = html_entity_decode($content);
 
-    $store_hours    = get_post_by_title( "Store Hours", "content", "post" ); 
+    // $store_hours    = get_post_by_title( "Store Hours", "content", "post" ); 
+    $days   = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
+    $display_shop_hours = get_option( 'display_shop_hours' ); 
 
     ob_start(); ?>
         <section class="store-front-open">
             <div class="store-front-open__inner max-wrapper__narrow">
                 <h2>Store Front Open</h2>
                 <div class="store-hours">
-                    <?php echo $store_hours; ?>
+                    <ul class="store-hours__days">
+                        <?php
+                            for( $i = 0; $i < count($days); $i++ )
+                            {
+                                if( empty( $display_shop_hours["'o'"][$i] ) ) continue;
+                                ?>
+                                <li>
+                                    <?php echo $days[$i]; ?>: <?php echo $display_shop_hours["'o'"][$i]; ?> - <?php echo $display_shop_hours["'c'"][$i]; ?>
+                                </li>
+                                <?php
+                            }
+                        ?>
+                    </ul>
                 </div>
-                <?php echo $content; ?>
+                <p><?php echo $content; ?></p>
                 <div class="cta--wrapper">
                     <a href="<?php echo site_url(); ?>/online-shop" class="button button-link">Online Store</a>
                 </div>
