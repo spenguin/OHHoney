5437749b67909aefe154988af82d5a5e31602acb
John Anderson <info@soaringpenguin.com>
2025-07-25 15:25:53 -0700

    Echo available dates for collection.

--- includes/wcextension.php
+++ includes/wcextension.php
@@ -69,5 +69,90 @@ function render_slideshow()
  */
 function oh_select_collection_date()
 {
-    echo '<p>Select collection date</p>';
+    $dates = getDatesForPeriod(date('Y-m-d'), '60' );
+    $availableDates = [];
+    
+    // Start with Shop Hours
+    $display_shop_hours = get_option( 'display_shop_hours' ); 
+
+    foreach( $dates as $date )
+    {   
+        $available  = empty($display_shop_hours["'c'"][date('w', strtotime($date))]) ? '' : 'In Store';
+        $availableDates[$date]  = $available;
+    }
+    
+    // Now work through all Market dates
+
+    $args = [
+        'post_type'         => 'market',
+        'posts_per_page'    => -1
+    ];
+
+    $query = new WP_Query( $args );
+    
+    if( $query->have_posts()): while($query->have_posts()): $query->the_post();
+        $market_title   = get_the_title();
+        $market_date_time = get_post_meta( $query->post->ID, 'market_date_time', TRUE );
+        foreach( $market_date_time as $m ): 
+            if( empty($m["'date'"] ) ) continue;
+            $market_date = date('Y-m-d', strtotime($m["'date'"]));
+            if( array_key_exists($market_date, $availableDates ))
+            {
+                $availableDates[$market_date]   = $market_title;
+            }
+        endforeach;
+
+    endwhile; endif; wp_reset_postdata();
+    
+    // ob_start(); ?>
+    <ul>
+        <?php    
+            foreach( $availableDates as $date => $location )
+            { ?>
+                <li>
+                    <?php
+                        if( !empty($location) ): ?>
+                            <input type="checkbox" name="selectedDate" value="<?php echo $date; ?>" />
+                    <?php endif; ?>
+                    <?php echo $date; ?>
+                    <?php
+                        if( !empty($location) ): ?>
+                            - <?php echo $location; ?>
+                    <?php endif; ?>                    
+                </li>
+                <?php
+            }
+    ?></ul><?php
+    // return ob_get_clean();
 }
+
+// $dateString = '2024-07-25'; // Replace with your desired date
+// $timestamp = strtotime($dateString); // Convert the date string to a Unix timestamp
+// $dayOfWeekIndex = date('w', $timestamp); // Get the day of the week as an index (0 for Sunday, 6 for Saturday)
+
+// echo "The day of the week index for " . $dateString . " is: " . $dayOfWeekIndex;
+
+/** 
+ * Get all Dates from Start Date for specified period
+ */
+
+function getDatesForPeriod($startDate, $period) 
+{
+    $dates = [];
+    $currentDate = new DateTime($startDate); 
+    $endDate = new DateTime($startDate . "+" . $period . "days"); 
+
+    while ($currentDate <= $endDate) {
+        $dates[] = $currentDate->format('Y-m-d');
+        $currentDate->modify('+1 day');
+    }
+
+    return $dates;
+}
+
+// // Example usage:
+// $startDate = '2025-07-01';
+// $endDate = '2025-07-05';
+// $allDays = getDatesBetween($startDate, $endDate);
+
+// print_r($allDays);
