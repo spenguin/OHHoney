7e9a4bc0e23041cd5abf63edf8c858b3ffad6486
spenguin <inof@soaringpenguin.com>
2026-04-25 16:25:34 -0700

    Add the shops to the navigation.

--- template-parts/header-navigation.php
+++ template-parts/header-navigation.php
@@ -6,6 +6,7 @@
     <ul id="menu-primary" class="menu">
         <li ><a href="https://oh.weirdspace.xyz/online-shop/">Shop</a></li>
         <!-- <li ><a href="https://oh.weirdspace.xyz/recipes/">Recipes</a></li> -->
+        <li ><a href="https://oh.weirdspace.xyz/shops-i-supply/">Shops I Supply</a></li>
         <li ><a href="https://oh.weirdspace.xyz#markets">Markets</a></li>
         <li ><a href="https://oh.weirdspace.xyz/contact/">Contact</a></li>
     </ul>
